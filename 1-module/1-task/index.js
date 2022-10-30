3627ba4a993265c92249d1e2d25f6351277fdea6
Artem Kuteynikov <del-ry4me@yandex.ru>
2022-10-30 23:10:52 +0300

    solution of task_1_1

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,13 @@
 function factorial(n) {
   // ваш код...
+  if (n === 1 || n === 0) {
+    return 1;
+  }
+  let result = n;
+  let i = 1;
+  while (i < n) {
+    result = result * (n - i);
+    i = i + 1;
+  }
+  return result;
 }
