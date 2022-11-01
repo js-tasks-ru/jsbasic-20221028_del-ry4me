54a802d8ba7a6c9624fc77f09d4e39b09996e65a
Artem Kuteynikov <del-ry4me@yandex.ru>
2022-11-01 20:48:34 +0300

    solution of 3_task

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,14 @@
 function sumSalary(salaries) {
   // ваш код...
+  let result = 0;
+
+  for (let key in salaries) {
+
+    if (Number.isInteger(salaries[key])) {
+
+      result = result + salaries[key];
+    }
+  }
+
+  return result;
 }
