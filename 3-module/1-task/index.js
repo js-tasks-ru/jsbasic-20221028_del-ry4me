1743039a640e8c264af317e7b0ee95c8618357bd
Artem Kuteynikov <del-ry4me@yandex.ru>
2022-11-06 12:58:18 +0300

    solution of task 3_1_to_5

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -1,3 +1,8 @@
 function namify(users) {
   // ваш код...
+  let result = [];
+  for (const item of users) {
+    result.push(item.name);
+  }
+  return result;
 }
--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,25 @@
 function getMinMax(str) {
   // ваш код...
+  let arr = [];
+  let arrNumbers = [];
+  let max = 0;
+  let min = 0;
+  let result = {};
+
+  arr = str.split(' ');
+
+  for (const item of arr) {
+
+    if (!Number.isNaN(+item)) {
+      arrNumbers.push(item);
+    }
+  }
+  max = Math.max(...arrNumbers);
+
+  min = Math.min(...arrNumbers);
+
+  result = {min, max};
+
+  return result;
 }
+
